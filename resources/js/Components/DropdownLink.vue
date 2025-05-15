a600ff789b1889c70f927a1b315bdc1518b73ba9
Dendi Aprilio Setiawan <gelasajur1209@gmail.com>
2025-05-15 23:42:45 +0700

    migrasi

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -8,7 +8,36 @@ defineProps({
     },
 });
 </script>
+<script setup>
+import { Link } from '@inertiajs/vue3';
 
+defineProps({
+    href: {
+        type: String,
+        required: true,
+    },
+    activeClass: {
+        type: String,
+        default: 'active',
+    },
+});
+
+const isActive = computed(() => {
+    return window.location.pathname === href;
+});
+</script>
+
+<template>
+    <Link
+        :href="href"
+        :class="[
+            'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out',
+            { [activeClass]: isActive },
+        ]"
+    >
+        <slot />
+    </Link>
+</template>
 <template>
     <Link
         :href="href"
